2988768d286116fba4d3bb1dfb574ca13495eee2
islom valiyev <islomvaliyev30@gmail.com>
2023-08-18 18:54:35 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,15 @@ import { useState } from 'react'
 import './App.css'
 import Cart from './Components/Cart';
 import ProdList from './Components/ProdList';
+import {FiShoppingCart} from 'react-icons/fi'
+import {BiSearchAlt2} from 'react-icons/bi'
+
 
 const tg = window.Telegram.WebApp;
 
 function App() {
 
+  const [input1, setInput1] = useState('')
   const [mood, setMood] = useState(false)
 
   const [arr, setArr] = useState([
@@ -27,10 +31,14 @@ function App() {
 
   return (
     <div className='mainCss'>
-      <span onClick={() => setMood(!mood)}>Cart</span>
-      <>
-        {!mood?<ProdList arr={arr} setBasket={setBasket}/>:mood?<Cart basket={basket} setBasket={setBasket}/>:null}
-      </>
+      <div className='header'>
+        <div className='input1'>
+          <span className='icon1'><BiSearchAlt2/></span>
+          <input type="search" value={input1} onChange={(ev) => setInput1(ev.target.value)}/>
+        </div>
+        <span className='icon1' onClick={() => setMood(!mood)}><FiShoppingCart/></span>
+      </div>
+      {!mood?<ProdList arr={arr} setBasket={setBasket}/>:mood?<Cart basket={basket} setBasket={setBasket}/>:null}
       <button onClick={() => CloseBot()}>Close</button>
     </div>
   )
