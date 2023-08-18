b6cb4f7b994f10e2ea959e84ac7ab1e0283513c7
islom valiyev <islomvaliyev30@gmail.com>
2023-08-18 10:59:10 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,14 @@ import './App.css'
 const tg = window.Telegram.WebApp;
 
 function App() {
+
+  const [arr, setArr] = useState([
+    {name: 'Big Mac', price: '10.0', type: 'burger', img: ''},
+    {name: 'Double Mac', price: '12.0', type: 'burger', img: ''},
+    {name: 'Papperoni-Big', price: '16.0', type: 'pizza', img: ''},
+    {name: 'Cheezes-Big', price: '14.0', type: 'pizza', img: ''}
+  ])
+  const [basket, setBasket] = useState([])
   useEffect(() => {
     tg.ready()
   }, [])
@@ -13,9 +21,24 @@ function App() {
     tg.close()
   }
 
+  function Order(item){
+    setBasket(item)
+  }
+
   return (
     <div>
-      Works!
+      <span>Cart</span>
+      <div>
+      {
+        arr?.map((item, index) => {
+          return <div key={item.name}>
+            <h4>{item.name}</h4>
+            <span>{item.price}</span>
+            <button onClick={() => Order(item)}>Order</button>
+          </div>
+        })
+      }
+      </div>
       <button onClick={() => CloseBot()}>Close</button>
     </div>
   )
